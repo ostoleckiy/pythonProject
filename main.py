dcfebcdd18ab31006104901d2c8bf664d8f554c7
Ostoleckiy <ostoleckiy@ukr.net>
2021-02-25 21:18:10 +0200

    We gets propeties

---
+++ main.py
@@ -0,0 +1,10 @@
+import platform
+import sys
+
+info = 'OS info is \n{}\n\n Python version is {} {}'.format(
+    platform.uname(), sys.version, platform.architecture())
+print(info)
+
+with open('os_info.txt', 'w') as ff:
+    ff.write(info)
+    # зачет!
